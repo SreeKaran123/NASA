9b4424a33c45fe6cbdfc946d52919defd3153676
SreeKaran123 <karanamsreekaran2991@gmail.com>
2023-04-23 22:21:31 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Graph from './Components/Graph';
+
+function App() {
+  return (
+    <div className="App" style={{"background":"white"}}>
+      <Graph/>
+    </div>
+  );
+}
+
+export default App;
